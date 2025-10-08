d4dc04ffff4fcc920f2087560fa48a24213e483d
Vadim Sukhomlynov <97035738+Vadim-Sukhomlinov@users.noreply.github.com>
2025-10-08 18:30:19 +0300

    re fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/volonter-kommunarka/',
+  base: '/',
   plugins: [react()],
   assetsInclude: ['**/*.mp4', '**/*.mov', '**/*.avi'],
 })
